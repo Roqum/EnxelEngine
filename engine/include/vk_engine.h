6b7214917a821dc214f6a44a6a13c30ced1a8457
Roqum <david.burgstaller@yahoo.de>
2025-04-29 20:24:20 +0200

    clean up

---
+++ engine/include/vk_engine.h
@@ -0,0 +1,30 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+//> intro
+#pragma once
+
+
+class VulkanEngine {
+public:
+
+	bool _isInitialized{ false };
+	int _frameNumber {0};
+	bool stop_rendering{ false };
+
+	struct SDL_Window* _window{ nullptr };
+
+	static VulkanEngine& Get();
+
+	//initializes everything in the engine
+	void init();
+
+	//shuts down the engine
+	void cleanup();
+
+	//draw loop
+	void draw();
+
+	//run main loop
+	void run();
+};
+//< intro
---
+++ engine/src/vk_engine.cpp
@@ -0,0 +1,37 @@
+﻿//> includes
+#include "vk_engine.h"
+#include "iostream"
+#include <chrono>
+#include <thread>
+//< includes
+
+//> init
+constexpr bool bUseValidationLayers = false;
+
+VulkanEngine* loadedEngine = nullptr;
+
+VulkanEngine& VulkanEngine::Get() { return *loadedEngine; }
+void VulkanEngine::init()
+{
+   
+}
+
+void VulkanEngine::cleanup()
+{
+    
+}
+
+void VulkanEngine::draw()
+{
+}
+
+void VulkanEngine::run()
+{
+    std::cout << "Hello, World!" << std::endl; 
+    while (true)
+    {
+
+    }
+    
+}
+//< drawloop
