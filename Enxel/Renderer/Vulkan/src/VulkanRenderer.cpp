8723fb0ac05c7587fc2a2d7bc435490546debabc
Roqum <david.burgstaller@yahoo.de>
2025-07-29 22:06:06 +0200

    add win ifdef to dll function export

--- Enxel/Renderer/Vulkan/src/VulkanRenderer.cpp
+++ Enxel/Renderer/Vulkan/src/VulkanRenderer.cpp
@@ -1,6 +1,7 @@
 #include "Vulkan/VulkanRenderer.h"
 #include <iostream>
 
+#ifdef _WIN32
 extern "C" __declspec(dllexport) IRenderer* CreateRenderer() {
     return new VulkanRenderer();
 }
@@ -8,7 +9,15 @@ extern "C" __declspec(dllexport) IRenderer* CreateRenderer() {
 extern "C" __declspec(dllexport) void DestroyRenderer(IRenderer* renderer) {
     delete renderer;
 }
+#elif
+extern "C" IRenderer* CreateRenderer() {
+    return new VulkanRenderer();
+}
 
+extern "C" void DestroyRenderer(IRenderer* renderer) {
+    delete renderer;
+}
+#endif
 void VulkanRenderer::Init()
 {
 	std::cout << "VulkanRenderer initialized." << std::endl;
