daa0a1355aa6aa02dca952ee81a10d0e3f897909
Roqum <david.burgstaller@yahoo.de>
2025-08-19 12:38:58 +0200

    save stage

--- Enxel/Renderer/Vulkan/Chunk.h
+++ Enxel/Renderer/Vulkan/Chunk.h
@@ -8,7 +8,20 @@
 
 namespace Enxel
 {
+	constexpr int CHUNK_SIZE = 32;
+	constexpr int CHUNK_VOLUME = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
+	constexpr int CHUNK_MASK = CHUNK_SIZE - 1;
+	constexpr int CHUNK_SHIFT_Y = 5; // log2(32)
+	constexpr int CHUNK_SHIFT_Z = 10; // log2(32*32)
+
+	/* struct Voxel {
+		uint8_t type;
+	};
+	*/
+
 	struct Chunk {
+		std::array<Voxel, CHUNK_VOLUME> voxels;
+
 
 		VertexBuffer* vertexBuffer;
 		IndexBuffer* indexBuffer;
@@ -16,6 +29,19 @@ namespace Enxel
 		static const int CHUNK_SIZE = 32;
 
 		uint8_t blocks[CHUNK_SIZE][CHUNK_SIZE][CHUNK_SIZE];
+
+		// x,y,z -> index
+
+		inline int toIndex(int x, int y, int z) {
+			return x | (y << CHUNK_SHIFT_Y) | (z << CHUNK_SHIFT_Z);
+		}
+
+		// index -> x,y,z
+		inline void toCoords(int idx, int& x, int& y, int& z) {
+			x = idx & CHUNK_MASK;
+			y = (idx >> CHUNK_SHIFT_Y) & CHUNK_MASK;
+			z = idx >> CHUNK_SHIFT_Z;
+		}
 
 		void generateChunk() {
 			for (int x = 0; x < CHUNK_SIZE; x++) {
